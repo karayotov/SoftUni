1c229dd32d1c12e9ce3d052e74ca1651feaa1ff5
karayotov <karayotov@gmail.com>
2017-09-29 20:14:43 +0300

    p05 data types and Variables

--- SoftUni-Java/ProgrammingFundamentals/src/L04DataTypesAndVariables/exercices/p03PracticeCharsAndStrings.java
+++ SoftUni-Java/ProgrammingFundamentals/src/L04DataTypesAndVariables/exercices/p03PracticeCharsAndStrings.java
@@ -3,6 +3,16 @@ package L04DataTypesAndVariables.exercices;
 public class p03PracticeCharsAndStrings {
     public static void main(String[] args) {
 
-        String softUni = "Software "
+        String softUni = "Software University";
+        char best = 'B';
+        char yellow = 'y';
+        char exe = 'e';
+        String love = "I love programming";
+
+        System.out.println(softUni);
+        System.out.println(best);
+        System.out.println(yellow);
+        System.out.println(exe);
+        System.out.println(love);
     }
 }
---
+++ SoftUni-Java/ProgrammingFundamentals/src/L04DataTypesAndVariables/exercices/p05booleanVar.java
@@ -0,0 +1,11 @@
+package L04DataTypesAndVariables.exercices;
+
+import java.util.Scanner;
+
+public class p05booleanVar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean yesOrNo = Boolean.parseBoolean(sca)
+    }
+}
