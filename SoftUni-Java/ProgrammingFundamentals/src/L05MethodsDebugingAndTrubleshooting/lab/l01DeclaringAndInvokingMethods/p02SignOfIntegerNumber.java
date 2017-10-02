727832c52e4c5af3a1e408741f52946fdd58c7dd
karayotov <karayotov@gmail.com>
2017-10-02 18:27:34 +0300

    L05 lab to p03 Update

--- SoftUni-Java/ProgrammingFundamentals/src/L05MethodsDebugingAndTrubleshooting/lab/l01DeclaringAndInvokingMethods/p02SignOfIntegerNumber.java
+++ SoftUni-Java/ProgrammingFundamentals/src/L05MethodsDebugingAndTrubleshooting/lab/l01DeclaringAndInvokingMethods/p02SignOfIntegerNumber.java
@@ -1,4 +1,24 @@
 package L05MethodsDebugingAndTrubleshooting.lab.l01DeclaringAndInvokingMethods;
 
+import java.util.Scanner;
+
 public class p02SignOfIntegerNumber {
+//        int n = Integer.parseInt(scanner.nextLine());
+public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    int num = Integer.parseInt(scanner.nextLine());
+    PrintSign(num);
+
+}
+    static void PrintSign(int num){
+
+        if (num > 0){
+            System.out.printf("The number %d is positive.", num);
+        }else if (num < 0){
+            System.out.printf("The number %d is negative.", num);
+        }else {
+            System.out.printf("The number %d is zero.", num);
+        }
+    }
 }
