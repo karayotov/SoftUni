1fb46659d8d1aa001300a4f101dae2f4ba6cd8ad
karayotov <karayotov@gmail.com>
2017-11-06 19:18:39 +0200

    Exam_05_November_2017.Part_I.Part_I P01 100/100
    Exam_05_November_2017.Part_I.Part_I
    P01 100/100

--- SoftUni-Java/ProgrammingFundamentals/src/Exam_05_November_2017/Part_I/Part_I/ss.java
+++ SoftUni-Java/ProgrammingFundamentals/src/Exam_05_November_2017/Part_I/Part_I/ss.java
@@ -1,7 +1,5 @@
 package Exam_05_November_2017.Part_I.Part_I;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,32 +7,23 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
 
 public class ss {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(bufferedReader.readLine());
-        int securityKey = Integer.parseInt(bufferedReader.readLine());
-        int securityToken = 1;
+        BigInteger securityKey = new BigInteger(bufferedReader.readLine());
 
-        for (int i = 0; i < n; i++) {
-            securityToken *= securityKey;
-        }
+        BigInteger securityToken = securityKey.pow(n);
 
-
-        List<String> allNames = new LinkedList<>();
         List<BigDecimal> totalLoss = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             String[] inputArr = bufferedReader.readLine().split(" ");
             String siteName = inputArr[0];
 
-            allNames.add(siteName);
 
             BigDecimal siteVisits = new BigDecimal(inputArr[1]);
             BigDecimal siteCommercialPricePerVisit = new BigDecimal(inputArr[2]);
@@ -42,12 +31,10 @@ public class ss {
             BigDecimal perSite = siteCommercialPricePerVisit.multiply(siteVisits);
             totalLoss.add(perSite);
 
-            allNames.add(siteName);
+            System.out.println(siteName);
         }
-        System.out.println();
         BigDecimal sum = totalLoss.stream()
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
-        String[] sumStr = sum.toString().split(".");
 
         DecimalFormat df = new DecimalFormat("#0.00000000000000000000");
 
